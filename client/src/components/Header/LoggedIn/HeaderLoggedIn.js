a475b829e1a4559878089d99e3e11af3d771a624
MarkusAleksander <markholden1989@googlemail.com>
2020-12-05 16:45:03 +0000

    clientside setup

---
+++ client/src/components/Header/LoggedIn/HeaderLoggedIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Header from "./../../UI/Header/Header";
+
+const HeaderLoggedIn = (props) => <Header><div>Logged in header</div></Header>
+
+export default HeaderLoggedIn;
