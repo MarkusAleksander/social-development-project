fad24fa29c56198804a3e0c096d631191c2bcaed
MarkusAleksander <markholden1989@googlemail.com>
2020-12-05 12:38:01 +0000

    added home route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const app = express();
 // * set the port we're listening on
 const PORT = 8080;
 
+// * create a home web route
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
 // * Set up listening for connections
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
